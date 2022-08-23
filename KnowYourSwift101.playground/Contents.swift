0f5256e199bf0f380e60e1daed345aa7073a8ead
Ayemere <davidodia4@gmail.com>
2022-08-23 23:50:54 +0200

    first commit

---
+++ KnowYourSwift101.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+// INTEGERS
+// 8,16,32,64 signed (+ve 0) and unsigned (+ve 0 -ve)
+/*print("### UNSIGNED VALUES ####")
+
+let minimum8 = UInt8.min
+let maximum8 = UInt8.max
+print("UInt8 minimum: \(minimum8) maximum: \(maximum8)")
+let minimum16 = UInt16.min
+let maximum16 = UInt16.max
+print("UInt16 minimum: \(minimum16) maximum: \(maximum16)")
+
+let minimum32 = UInt32.min
+let maximum32 = UInt32.max
+print("UInt32 minimum: \(minimum32) maximum: \(maximum32)")
+
+let minimum64 = UInt64.min
+let maximum64 = UInt64.max
+print("UInt64 minimum: \(minimum64) maximum: \(maximum64)")
+
+print("### SIGNED VALUES ####")
+
+let sminimum8 = Int8.min
+let smaximum8 = Int8.max
+print("Int8 minimum: \(sminimum8) maximum: \(smaximum8)")
+let sminimum16 = Int16.min
+let smaximum16 = Int16.max
+print("Int16 minimum: \(sminimum16) maximum: \(smaximum16)")
+
+let sminimum32 = Int32.min
+let smaximum32 = Int32.max
+print("Int32 minimum: \(sminimum32) maximum: \(smaximum32)")
+
+let sminimum64 = Int64.min
+let smaximum64 = Int64.max
+print("Int64 minimum: \(sminimum64) maximum: \(smaximum64)")*/
+
+let stringSpecial = #"Hello Mike and \#n Amy WineHouse"#
+
+print(stringSpecial.index(stringSpecial.startIndex, offsetBy: <#T##String.IndexDistance#>))
